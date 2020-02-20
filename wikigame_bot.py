42017491ea8e8da42afaf8e7a9a0ae158c8d4063
Matthew Di Marco <matthew.d.dimarco@gmail.com>
2020-02-20 11:09:12 +0800

    Broke up Page into Page and Bot classes, Added clicks counter, Modified read function to instantly jump to links that match the target

--- wikigame_bot.py
+++ wikigame_bot.py
@@ -1,7 +1,6 @@
 import sys
 import random
 import time
-import traceback
 import requests as req
 from bs4 import BeautifulSoup as soup
 from nltk.corpus import wordnet
@@ -42,14 +41,15 @@ def main():
             print(CGRE + "VALID" + CEND + "\n")
             
             # if no exceptions are thrown, start main loop (play game)
-            target_name = target_page.get_name()
-            counter = TURNS
+            bot = Bot(target_page)    
+            counter = TURNS    
             curr_page = start_page
             
             start_seconds = time.time() # start timer
-            while (not curr_page.url == target_page.url) and (counter > 0):
-                successor_link = curr_page.analyse_links(target_name)
+            while (not curr_page.url.lower() == target_page.url.lower()) and (counter > 0):
+                successor_link = bot.read(curr_page)
                 print("moving to... {}".format(successor_link))
+                bot.clicks += 1
                 curr_page = Page(successor_link)
                 counter = counter - 1
                 
@@ -59,21 +59,98 @@ def main():
             else:
                 print(CRED + "Bot fails: No turns remain..." + CEND)
                 
-            print("Time: {} seconds".format(playtime))
+            print("Time:  {} seconds".format(playtime))
+            print("Steps: {} clicks".format(bot.clicks))
                                 
     except (ValueError, IndexError) as e:
         print(CRED + "{}".format(e) + CEND)
-        #print(traceback.print_exc())
+
+class Bot:
+    def __init__(self, target_page):
+        self.clicks = 0
+        self.target = target_page
+        
+    def read(self, article):
+        """
+        Analyse links for keywords. 
+        Guesstimates and returns link with best similarity to target.
+        """
+        links = article.scrape_links()
+        best_article = (0,0) # tuple for tracking best link: (index,score)
+        
+        target = self.target.get_name()
+        article_found = False
+        ii = 0
+        while ii < len(links) and not article_found:
+            if links[ii].lower() == target.lower():
+                article_found = True
+                best_article = (ii,1.0)
+            else:
+                score = self.__confidence(links[ii], target) 
+    
+                # guesstimate (i.e. add x% randomness to link selection to avoid infinite loops)
+                PERCENT_VARIANCE = 5 #%
+                margin = (PERCENT_VARIANCE / 100) * score
+                score += random.uniform(-margin, margin)
+                
+                if score > best_article[1]:
+                    best_article = (ii,score)
+                elif score == best_article[1]: # swap articles? toss a coin
+                    if random.randint(0,1) == 1:
+                        best_article = (ii,score)
+                
+            ii = ii + 1
+        
+        return links[best_article[0]]
+    
+    def __confidence(self, str1, str2):
+        """
+        Returns the bot's 'confidence' in an article as a decimal between 0 and 1.
+        
+        Breaks up str1 and str2 into 2 lists of words. A cross-product comparison
+        is carried out between the 2 lists, and words similar or identical in
+        syntax / meaning are picked out. The highest calculated similarity
+        from the wordnet function is then returned.
+        
+        Dependencies: nltk.corpus - wordnet
+        For this bot to work, run the following:
+            >>> import nltk
+            >>> nltk.download() 
+        """   
+        best_confidence = 0
+        syns = wordnet.synsets("program")
+        str1_words = str1.lower().split('_')
+        str2_words = str2.lower().split('_')
+        
+        for ii in range(len(str1_words)):
+            try:
+                word1 = wordnet.synset(''.join((str1_words[ii], '.n.01')))
+                for jj in range(len(str2_words)):
+                    try:
+                        word2 = wordnet.synset(''.join((str2_words[jj], '.n.01')))
+                        confidence = word1.wup_similarity(word2)
+                        
+                        if confidence > best_confidence:
+                            best_confidence = confidence
+                            
+                    except Exception as e:
+                        #for WordNetError exceptions
+                        #If word couldn't be found, skip to next word
+                        pass
+                    
+            except Exception as e:
+                #for WordNetError exceptions
+                #If word couldn't be found, skip to next word
+                pass
+            
+        return best_confidence
 
 class Page:
-    def __init__(self, in_url):        
-        self.url = ''.join((WIKIPEDIA, in_url))
+    def __init__(self, name):        
+        self.url = ''.join((WIKIPEDIA, name))
         
     def exists(self):
-        """
-        Returns true if the url is a real article.
-        Should call after construction if unsure of article validity.
-        """
+        """Returns true if the url is a real article."""
         exists = True
         msg = (
             'Wikipedia does not have an article with this exact name.'
@@ -89,87 +166,8 @@ class Page:
                 exists = False
         
         return exists
-                
-    def analyse_links(self, target):
-        """Returns article with highest % similarity score to the target article"""
-        links = self.__scrape_links()
-        best_article = (0,0) # tuple for tracking best link: (index,score)
-        
-        for ii in range(len(links)):
-            score = self.__similarity(links[ii], target) 
     
-            # guesstimate (i.e. add x% randomness to link selection to avoid infinite loops)
-            PERCENT_VARIANCE = 10 #%
-            margin = (PERCENT_VARIANCE / 100) * score
-            score += random.uniform(-margin, margin)
-            
-            if score > best_article[1]:
-                best_article = (ii,score)
-        
-        return links[best_article[0]]
-    
-    def get_name(self):
-        """Returns the name of the article without the url"""
-        return self.url.split("/")[4]
-    
-    def display(self):
-        """Print out this page's url plus all it's links"""
-        print("This page: {}".format(self.url))
-        links = self.__scrape_links()
-        for ii in range(len(links)):
-            print(links[ii])
-    
-    ##
-    # PRIVATES
-    ##
-    
-    def __similarity(self, str1, str2):
-        """
-        Returns the similarity between 2 strings as a decimal between 0 and 1.
-        
-        Method: 1. Break each string up into a list of words (split @ '_' symbol).
-                2. Do a cross-product comparison of each word from each list using
-                   'Lemmatisation' to calculate the similarity between the 2 words.
-                3. Out of all the comparisons, the highest calculated value will
-                   be returned (pick out the keywords).
-                   
-        Dependencies: nltk.corpus - wordnet
-        For this bot to work, run the following:
-            >>> import nltk
-            >>> nltk.download() 
-        """   
-        best_confidence = 0
-        if str1.lower() == str2.lower():
-            best_confidence = 1.0
-        else:
-            syns = wordnet.synsets("program")
-            str1_words = str1.lower().split('_')
-            str2_words = str2.lower().split('_')
-            
-            for ii in range(len(str1_words)):
-                try:
-                    word1 = wordnet.synset(''.join((str1_words[ii], '.n.01')))
-                    for jj in range(len(str2_words)):
-                        try:
-                            word2 = wordnet.synset(''.join((str2_words[jj], '.n.01')))
-                            confidence = word1.wup_similarity(word2)
-                            
-                            if confidence > best_confidence:
-                                best_confidence = confidence
-                                
-                        except Exception as e:
-                            #for WordNetError exceptions
-                            #If word couldn't be found, skip to next word
-                            pass
-                        
-                except Exception as e:
-                    #for WordNetError exceptions
-                    #If word couldn't be found, skip to next word
-                    pass
-            
-        return best_confidence
-    
-    def __scrape_links(self):
+    def scrape_links(self):
         """Returns a list of article links within this body's article"""
         bod_soup = self.__get_body()       
         links_soup = bod_soup.findAll("a", {
@@ -179,14 +177,25 @@ class Page:
         links = []
         for ii in range(len(links_soup)):
             if (  
-                not links_soup[ii]["href"].endswith(".jpg") and \
-                not links_soup[ii]["href"].endswith(".png") and \
-                not len(links_soup[ii]["href"]) > 30 #TODO: consider changing this...
+                not links_soup[ii]["href"].lower().endswith(".jpg") and \
+                not links_soup[ii]["href"].lower().endswith(".png") and \
+                not len(links_soup[ii]["href"]) > 30
             ): 
                 # convert soup to string and take just the name (dump '/wiki/')
                 links.append(links_soup[ii]["href"].split("/")[2])
                 
         return links
+    
+    def get_name(self):
+        """Returns the name of the article without the url"""
+        return self.url.split("/")[4]
+    
+    def display(self):
+        """Print out this page's url plus all it's links"""
+        print("This page: {}".format(self.url))
+        links = self.scrape_links()
+        for ii in range(len(links)):
+            print(links[ii])
     
     def __get_body(self):
         """Returns the body content of an article as a soup obj"""
